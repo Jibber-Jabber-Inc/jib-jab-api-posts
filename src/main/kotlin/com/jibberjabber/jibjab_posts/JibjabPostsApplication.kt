03a68cc75ef91c674480c0f676341806e566b26d
joacoiannuzzi <joaquin.iannuzzi@ing.austral.edu.ar>
2021-05-07 00:50:34 -0300

    initial commit

---
+++ src/main/kotlin/com/jibberjabber/jibjab_posts/JibjabPostsApplication.kt
@@ -0,0 +1,11 @@
+package com.jibberjabber.jibjab_posts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JibjabPostsApplication
+
+fun main(args: Array<String>) {
+	runApplication<JibjabPostsApplication>(*args)
+}
---
+++ src/test/kotlin/com/jibberjabber/jibjab_posts/JibjabPostsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jibberjabber.jibjab_posts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JibjabPostsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
