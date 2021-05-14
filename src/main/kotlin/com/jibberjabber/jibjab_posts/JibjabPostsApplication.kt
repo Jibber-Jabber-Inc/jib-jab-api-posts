a0abb58b0ca50cd0c639d9e6be6cc8cd90b38bef
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-05-13 23:08:13 -0300

    add cross origin and header validations

--- src/main/kotlin/com/jibberjabber/jibjab_posts/JibjabPostsApplication.kt
+++ src/main/kotlin/com/jibberjabber/jibjab_posts/JibjabPostsApplication.kt
@@ -2,10 +2,28 @@ package com.jibberjabber.jibjab_posts
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableScheduling
 class JibjabPostsApplication
 
 fun main(args: Array<String>) {
-	runApplication<JibjabPostsApplication>(*args)
+    runApplication<JibjabPostsApplication>(*args)
+}
+
+@Bean
+fun corsConfigurer(): WebMvcConfigurer {
+    return object : WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry.addMapping("*").allowedOrigins("*").allowedHeaders("*")
+        }
+    }
 }
