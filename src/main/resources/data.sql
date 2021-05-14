e3daed6c324b9f791aeb32783a815e95c0b39122
MaxiAdaro <maximiliano.adaro@ing.austral.edu.ar>
2021-05-13 22:48:18 -0300

    insert default values

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO post (id, creation_date, description)
+VALUES (floor(random() * 1000000), '2021-05-13 21:46:33.991527', 'hola mundo 1'),
+       (floor(random() * 1000000), '2021-05-14 21:46:35.047895', 'hola mundo 2'),
+       (floor(random() * 1000000), '2021-05-15 21:46:35.789087', 'hola mundo 3'),
+       (floor(random() * 1000000), '2021-05-15 22:46:36.472464', 'hola mundo 4');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table if not exists post(
+    id varchar primary key,
+    description varchar,
+    creation_date timestamp
+);
